ca754166fad1378a0bcda7ddcb44807493d72d41
Dev-Sooraj-Git <mackarm731@gmail.com>
2026-05-08 23:44:53 +0530

    project model created & rules written

---
+++ app/Models/Project.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Project extends Model
+{
+    protected $fillable = [
+        'user_id',
+        'title',
+        'slug',
+        'description',
+        'client_name',
+        'client_email',
+        'total_amount',
+        'status',
+        'started_at',
+        'completed_at'
+    ];
+
+    protected function casts()
+    {
+        return [
+            'total_amount' => 'decimal:2',
+            'started_at' => 'date',
+            'completed_at' => 'date'
+        ];
+    }
+
+    public function user(){
+       return $this->belongsTo(User::class);
+    }
+}
