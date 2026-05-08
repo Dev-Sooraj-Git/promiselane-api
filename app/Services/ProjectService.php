d754abba6c7d7b3e84544956d5d606251398baa1
Dev-Sooraj-Git <mackarm731@gmail.com>
2026-05-09 00:00:27 +0530

    project service updated

--- app/Services/ProjectService.php
+++ app/Services/ProjectService.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Services;
+
 use App\Models\Project;
 use Illuminate\Support\Str;
 
@@ -10,8 +11,16 @@ class ProjectService
     /**
      * Create a new class instance.
      */
-    public function __construct()
+    public function List(int $userId, string $status = null)
     {
-        //
+        return Project::Where('user_id', $userId)
+            ->When($status, fn($q) => $q->where('status', $status))
+            ->latest()
+            ->get();
+    }
+
+    public function create(array $data){
+
+
     }
 }
