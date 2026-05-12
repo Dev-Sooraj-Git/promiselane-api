7a78565becea816d47d745437928a6a7b32a5d62
Dev-Sooraj-Git <mackarm731@gmail.com>
2026-05-12 14:52:03 +0530

    Add index method to controller

--- app/Http/Controllers/Api/V1/MilestoneController.php
+++ app/Http/Controllers/Api/V1/MilestoneController.php
@@ -4,8 +4,34 @@ namespace App\Http\Controllers\Api\V1;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Http\JsonResponse;
+use App\Models\Project;
+use App\Services\MilestoneService;
+use App\Http\Resources\MilestoneResource;
+
+
 
 class MilestoneController extends Controller
 {
-    //
+    protected MilestoneService $milestoneService;
+
+    public function __construct(MilestoneService $milestoneService)
+    {
+        $this->milestoneService = $milestoneService;
+        $this->middleware('auth:api');
+    }
+
+    public function index(Project $project)
+    {
+
+        $this->authorize('view', $project);
+        $milestones = $this->milestoneService->listByProject($project);
+
+        return response()->json(
+            [
+                'success' => true,
+                'data' => MilestoneResource::collection($milestones),
+            ]
+        );
+    }
 }
