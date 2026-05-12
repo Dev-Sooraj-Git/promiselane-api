26b6a63c249a79093b441f0f55ce624396a38651
Dev-Sooraj-Git <mackarm731@gmail.com>
2026-05-12 16:29:41 +0530

    Milestone Resource to Array Updated

---
+++ app/Http/Resources/MilestoneResource.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class MilestoneResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'project_id' => $this->project_id,
+            'title' => $this->title,
+            'description' => $this->description,
+            'amount' => $this->amount,
+            'due_date' => $this->due_date,
+            'status' => $this->status,
+            'order_index' => $this->order_index,
+            'paid_at' => $this->paid_at,
+            'created_at' => $this->created_at
+        ];
+    }
+}
