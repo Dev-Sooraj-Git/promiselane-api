823bb13f328e1efba8179d205e76d97a2c8c1b23
Dev-Sooraj-Git <mackarm731@gmail.com>
2026-05-16 20:00:09 +0530

    Add deliverable Controller

---
+++ app/Http/Controllers/Api/V1/DeliverableController.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\Http\Controllers\Controller;
+use App\Http\Resources\DeliverableResource;
+use App\Models\Deliverable;
+use App\Models\Milestone;
+use App\Services\DeliverableService;
+use Illuminate\Http\Request;
+
+class DeliverableController extends Controller
+{
+
+    protected DeliverableService $deliverableService;
+
+    public function __construct(DeliverableService $deliverableService)
+    {
+        $this->deliverableService = $deliverableService;
+        $this->middleware('auth:api');
+    }
+
+    public function index(Milestone $milestone)
+    {
+        $this->authorize('view', $milestone->project);
+
+        $deliverables = $this->deliverableService->listByMilestone($milestone);
+        return response()->json([
+            'success' => true,
+            'data' => DeliverableResource::collection($deliverables)
+        ]);
+    }
+
+    public function store(Request $request, Milestone $milestone)
+    {
+        $this->authorize('update', $milestone->project);
+
+        $request->validate([
+            'file_name' => 'required|string',
+            'path' => 'required|string',
+            'mime_type' => 'required|string',
+            'size' => 'required|integer',
+            'notes' => 'nullable|string'
+        ]);
+
+        $deliverable = $this->deliverableService->create($milestone, $request->only([
+            'file_name',
+            'path',
+            'mime_type',
+            'size',
+            'notes'
+        ]));
+
+        return response()->json([
+            "success" => true,
+            "message" => 'Deliverables Created',
+            "data" => new DeliverableResource($deliverable)
+        ], 201);
+    }
+
+    public function destroy(Milestone $milestone, Deliverable $deliverable)
+    {
+        $this->authorize('delete', $milestone->project);
+        $this->deliverableService->delete($deliverable);
+        return response()->json([
+            "success" => true,
+            "message" => "Deliverable Deleted"
+        ]);
+    }
+}
