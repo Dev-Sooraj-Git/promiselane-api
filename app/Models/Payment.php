34ae2820130e5207bf1a794ebf3033c616f5c552
Dev-Sooraj-Git <mackarm731@gmail.com>
2026-06-18 20:39:56 +0530

    Refine payment model casts

--- app/Models/Payment.php
+++ app/Models/Payment.php
@@ -8,7 +8,7 @@ class Payment extends Model
 {
     protected $fillable = ['milestone_id', 'amount', 'paid_at', 'method', 'reference', 'notes'];
 
-    public function casts(){
+    protected function casts(){
         return [
             'amount' => 'decimal:2',
             'paid_at' => 'date',
