4a9ca80e46971005fd7d6399e7f34c56b662dbde
Dev-Sooraj-Git <mackarm731@gmail.com>
2026-07-06 19:07:09 +0530

    forget password route correction done

--- routes/api.php
+++ routes/api.php
@@ -25,7 +25,7 @@ Route::prefix('v1')->group(function () {
     Route::prefix('auth')->group(function () {
         Route::post('register', [AuthController::class, 'register']);
         Route::post('login', [AuthController::class, 'login']);
-        Route::post('forgot-password', [PasswordResetController::class, 'forgetPassword'])->middleware('throttle:5,1');
+        Route::post('forgot-password', [PasswordResetController::class, 'forgotPassword'])->middleware('throttle:5,1');
         Route::post('reset-password', [PasswordResetController::class, 'resetPassword'])->middleware('throttle:5,1');
 
         Route::middleware('auth:api')->group(function () {
